a535cfb2dab3da3c53d8ae94d184af5b32971930
zackPC <zack09955189@gmail.com>
2023-03-24 18:09:38 +0800

    GUI Test

--- UML_Editor_Java_2023/src/Main.java
+++ UML_Editor_Java_2023/src/Main.java
@@ -1,5 +1,30 @@
-public class Main {
+//public class Main {
+//    public static void main(String[] args) {
+//        System.out.println("Hello world!");
+//    }
+//}
+
+import java.awt.*;
+import javax.swing.*;
+
+public class GUIDemo1 {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        JFrame demo = new JFrame();
+        demo.setSize(400, 300);
+        demo.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JCheckBox checkbox = new JCheckBox("JCheckBox");
+        JRadioButton radiobutton = new JRadioButton("JRadiobutton");
+        JButton button = new JButton("JButton");
+        JLabel label = new JLabel("JLabel");
+        JTextArea textarea = new JTextArea("JTextArea");
+
+        demo.getContentPane().add(BorderLayout.EAST, checkbox);
+        demo.getContentPane().add(BorderLayout.WEST, radiobutton);
+        demo.getContentPane().add(BorderLayout.SOUTH, button);
+        demo.getContentPane().add(BorderLayout.NORTH , label);
+        demo.getContentPane().add(BorderLayout.CENTER, textarea);
+
+        demo.setVisible(true);
     }
 }
